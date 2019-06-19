d1830b88efcc4fe6c5d6edee9e68a1f1d01c14a3
sbx_user1051 <sbx_user1051@169.254.130.133>
2019-06-19 17:43:11 +0000

    Wed Jun 19 17:43:11 UTC 2019

---
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -0,0 +1,21 @@
+package befaster.solutions.CHK;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckoutSolutionTest {
+
+    CheckoutSolution checkoutSolution;
+
+    @Before
+    public void setUp() throws Exception {
+        checkoutSolution = new CheckoutSolution();
+    }
+
+    @Test
+    public void checkout() {
+
+    }
+}
