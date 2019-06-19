e7b92a30ed048d3409832814ad3554bf1d4051f0
sbx_user1051 <sbx_user1051@169.254.130.133>
2019-06-19 18:07:11 +0000

    Wed Jun 19 18:07:11 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,7 +7,8 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
 
         String[] split = skus.split("");
-
+        
         return Arrays.stream(split).map(Product::valueOf).map(Product::getPrice).reduce(0, Integer::sum);
     }
 }
+
